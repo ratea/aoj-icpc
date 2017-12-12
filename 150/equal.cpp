625d35142a9f1ec3c26a9b2b663f0a2f376e6adf
genki.t <roma3983@gmail.com>
2017-12-13 03:56:56 +0900

    Add files via upload

---
+++ 150/equal.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int a,b,c;
+int np;
+int ans=0;
+
+int dfs(int x,int d){
+	d++;
+	if(d<b){
+		for(int i=1+x;i<=a;i++){
+			np+=i;
+			dfs(i,d);
+			np-=i;
+		}
+	}else{
+		if(np==c)ans++;
+	}
+	return 0;
+}
+
+int main(){
+	while(cin>>a>>b>>c){
+		ans=0;
+		if(a==0&&b==0&&c==0)break;
+		for(int i=0;i<=a;i++){
+			np=i;
+			dfs(i,0);
+		}
+		cout<<ans<<endl;
+	}
+	
+	return 0;
+}
