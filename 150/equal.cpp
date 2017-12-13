72a054cc27236ab4adb8a5dce2ecb6794c96011d
genki.t <roma3983@gmail.com>
2017-12-13 23:42:40 +0900

    Add files via upload

--- 150/equal.cpp
+++ 150/equal.cpp
@@ -7,14 +7,16 @@ int ans=0;
 
 int dfs(int x,int d){
 	d++;
+	if(d==b){
+		if(np==c)ans++;
+		return 0;
+	}
 	if(d<b){
-		for(int i=1+x;i<=a;i++){
+		for(int i=x+1;i<=a;i++){
 			np+=i;
 			dfs(i,d);
 			np-=i;
 		}
-	}else{
-		if(np==c)ans++;
 	}
 	return 0;
 }
@@ -23,12 +25,11 @@ int main(){
 	while(cin>>a>>b>>c){
 		ans=0;
 		if(a==0&&b==0&&c==0)break;
-		for(int i=0;i<=a;i++){
+		for(int i=1;i<=a;i++){
 			np=i;
 			dfs(i,0);
 		}
 		cout<<ans<<endl;
 	}
-	
 	return 0;
 }
